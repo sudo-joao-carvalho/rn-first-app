e1342dc1a611f8ddf8911000b6c11c7444e0af1f
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2024-02-23 12:05:55 +0000

    components add

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [goals, setGoals] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const addGoalHandler = (enteredGoal) => {
+    //setGoals((previousState) => [...previousState, enteredGoal]); -> for a normal array
+
+    //FlatList expects an array of objects because they need to have a key
+    setGoals((previousState) => [...previousState, {key: Math.random().toString(), value: enteredGoal}]);
+
+    /* setGoals((previousState) => [...previousState, {id: Math.random().toString(), value: enteredGoal}]);
+        but in the FlatList component I would have to add another property
+        keyExtractor={(item, index) => item.id}
+    */
+   setIsModalVisible(false);
+  }
+
+  const handleRemoveGoal = (goalToRemoveKey) => {
+    setGoals((previousState) => {
+      return previousState.filter((goal) => goal.key !== goalToRemoveKey);
+    });
+  }
+
+  const handleIsModalVisible = () => setIsModalVisible(!isModalVisible);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Button 
+        title={"Add new Goal"}
+        onPress={handleIsModalVisible}
+      />
+      <GoalInput
+        addGoalHandler={addGoalHandler}
+        isModalVisible={isModalVisible}
+        onCancel={handleIsModalVisible}
+      />
+      <FlatList 
+        data={goals}
+        renderItem={itemData => 
+          <GoalItem
+            goal={itemData.item}
+            handleRemoveGoal={handleRemoveGoal}
+          />
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
 });
